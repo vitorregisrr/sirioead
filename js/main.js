dc4a17f3a1fc88050211dcb1e0a577bdd6037e39
vitorregisr <vitorregisrr@gmail.com>
2019-06-18 16:27:17 -0300

    Add main-header fully responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 (function () {
     'use strict';
 
+    $('.main-header__tools__search').hover( function(){
+        $(this).closest('.main-header__tools').toggleClass('active');
+    });
+
     $('.headerlinks-toggler').click(() => {
         $('.headerlinks').toggleClass('active')
     });
