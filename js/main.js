d08862d91be507a7f69c7e37197b3af3d50f8378
vitorregisr <vitorregisrr@gmail.com>
2019-06-20 09:06:22 -0300

    Add footer, Add metodologia secton, Add new buttons, Misc improves

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 (function () {
     'use strict';
 
+    new WOW().init();
+
+
+    $('#btnProximosCursosToggler').click( function(){
+        $(this).toggleClass('active');
+        $('#proximos-cursos').toggleClass('active');
+        if( $('#proximos-cursos').hasClass('active') ){
+            $('html, body').animate({scrollTop:$('#proximos-cursos').position().top}, 'slow');
+        }
+    })
     $('.main-header__tools__search').hover( function(){
         $(this).closest('.main-header__tools').toggleClass('active');
     });
