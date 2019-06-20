e4f2facbb123565e3bb9f5d80065ae2cd7f5e398
vitorregisr <vitorregisrr@gmail.com>
2019-06-20 14:11:11 -0300

    Add page sobre, Add page-banner section, Add animations to header, Misc improves

--- js/main.js
+++ js/main.js
@@ -4,14 +4,36 @@
     new WOW().init();
 
 
-    $('#btnProximosCursosToggler').click( function(){
+    $('#btnProximosCursosToggler').click(function () {
         $(this).toggleClass('active');
         $('#proximos-cursos').toggleClass('active');
-        if( $('#proximos-cursos').hasClass('active') ){
-            $('html, body').animate({scrollTop:$('#proximos-cursos').position().top}, 'slow');
+        if ($('#proximos-cursos').hasClass('active')) {
+            $('html, body').animate({
+                scrollTop: $('#proximos-cursos').position().top
+            }, 'slow');
         }
     })
-    $('.main-header__tools__search').hover( function(){
+    $('.main-header__tools__search').hover(function () {
         $(this).closest('.main-header__tools').toggleClass('active');
     });
+
+
+    //scroll top
+    var scrollTopBtn = $('.scroll-top');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            scrollTopBtn.addClass('show');
+        } else {
+            scrollTopBtn.removeClass('show');
+        }
+    });
+
+    scrollTopBtn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+
 })();
