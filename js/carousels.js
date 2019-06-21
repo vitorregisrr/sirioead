cb413d3ef5510c7233a957d27cc0f7f09a61021a
vitorregisr <vitorregisrr@gmail.com>
2019-06-21 13:14:29 -0300

    Start curso page, Add curso page carousel fully responsive

--- js/carousels.js
+++ js/carousels.js
@@ -22,4 +22,25 @@
         animateIn: 'fadeIn'
     });
 
+     // Home Carousel //
+     const cursoCarousel = $(".owl-carousel.curso__carousel");
+     cursoCarousel.owlCarousel({
+         loop: false,
+         margin: 0,
+         items: 1,
+         center: true,
+         dots: true,
+         nav: true,
+         navText: [
+             "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
+         ],
+         slideBy: "page",
+         dragEndSpeed: 700,
+         smartSpeed: 1e3,
+         startPosition: 1,
+         mouseDrag: false,
+         animateOut: 'fadeOut',
+         animateIn: 'fadeIn'
+     });
+
 })();
