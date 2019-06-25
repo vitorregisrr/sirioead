65a825e591cdfec03114c6ed036f6c2e61deb297
vitorregisr <vitorregisrr@gmail.com>
2019-06-25 10:41:31 -0300

    Add curso v3, Misc improves

--- js/main.js
+++ js/main.js
@@ -53,17 +53,25 @@
 
 
     // Curso accordion
+    const updateAccordion = function () {
+        $('.curso__navAccordion__content').each(function (index, elem) {
+            const targetSelector = elem.getAttribute('data-cursoContent');
+            const targetContent = document.querySelector(targetSelector);
 
-    $('.curso__navAccordion__content').each(function (index, elem) {
-        const targetSelector =  elem.getAttribute('data-cursoContent');
-        const targetContent = document.querySelector(targetSelector);
+            if (targetContent) {
+                let newContent = document.createElement('div');
+                newContent.setAttribute('class', targetContent.getAttribute('class'));
+                newContent.innerHTML = targetContent.innerHTML;
+                elem.append(newContent);
+            }
 
-        if( targetContent ){
-            let newContent = document.createElement('div');
-            newContent.setAttribute('class', targetContent.getAttribute('class'));
-            newContent.innerHTML = targetContent.innerHTML;
-            elem.append(newContent);
-        }
-    });
+            elem.innerTML = '';
+            $('.curso__navAccordion').find('.wow').removeClass('wow');
+        });
+    }
+
+    if (window.innerWidth < 992) {
+        updateAccordion();
+    }
 
 })();
