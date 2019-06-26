d5fca94fe27ba7ab83977d721561b4aefe38ca45
vitorregisr <vitorregisrr@gmail.com>
2019-06-26 11:50:35 -0300

    Finish noticia page, Fix sobre links padding left

--- js/carousels.js
+++ js/carousels.js
@@ -22,47 +22,69 @@
         animateIn: 'fadeIn'
     });
 
-      // Noticias Carousel //
-      const noticiasCarousel = $(".owl-carousel.noticias__carousel");
-      noticiasCarousel.owlCarousel({
-          loop: false,
-          margin: 0,
-          items: 1,
-          center: true,
-          dots: true,
-          nav: true,
-          navText: [
-              "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
-          ],
-          slideBy: "page",
-          dragEndSpeed: 700,
-          smartSpeed: 1e3,
-          startPosition: 1,
-          mouseDrag: false,
-          animateOut: 'fadeOut',
-          animateIn: 'fadeIn'
-      });
+    // Noticias Carousel //
+    const noticiasCarousel = $(".owl-carousel.noticias__carousel");
+    noticiasCarousel.owlCarousel({
+        loop: false,
+        margin: 0,
+        items: 1,
+        center: true,
+        dots: true,
+        nav: true,
+        navText: [
+            "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
+        ],
+        slideBy: "page",
+        dragEndSpeed: 700,
+        smartSpeed: 1e3,
+        startPosition: 1,
+        mouseDrag: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn'
+    });
 
-      
-     // Curso Carousel //
-     const cursoCarousel = $(".owl-carousel.curso__carousel");
-     cursoCarousel.owlCarousel({
-         loop: false,
-         margin: 0,
-         items: 1,
-         center: true,
-         dots: true,
-         nav: true,
-         navText: [
-             "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
-         ],
-         slideBy: "page",
-         dragEndSpeed: 700,
-         smartSpeed: 1e3,
-         startPosition: 1,
-         mouseDrag: false,
-         animateOut: 'fadeOut',
-         animateIn: 'fadeIn'
-     });
+
+    // Noticia Carousel //
+    const noticiaCarousel = $(".owl-carousel.noticia__carousel");
+    noticiaCarousel.owlCarousel({
+        loop: false,
+        margin: 0,
+        items: 1,
+        center: true,
+        dots: true,
+        nav: true,
+        navText: [
+            "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
+        ],
+        slideBy: "page",
+        dragEndSpeed: 700,
+        smartSpeed: 1e3,
+        startPosition: 1,
+        mouseDrag: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn'
+    });
+
+
+    // Curso Carousel //
+    const cursoCarousel = $(".owl-carousel.curso__carousel");
+    cursoCarousel.owlCarousel({
+        loop: false,
+        margin: 0,
+        items: 1,
+        center: true,
+        dots: true,
+        nav: true,
+        navText: [
+            "<i class='owl-nav chevron-left'>", "<i class='owl-nav chevron-right'>"
+        ],
+        slideBy: "page",
+        dragEndSpeed: 700,
+        smartSpeed: 1e3,
+        startPosition: 1,
+        mouseDrag: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn'
+    });
 
 })();
